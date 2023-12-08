d874e0a6b8ccf2bd10c1a89edf6721760aea8076
Romero Maria Emilia <m.emiliaromero4@gmail.com>
2023-12-08 14:11:51 -0300

    Login page with different style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    layout :set_layout
+
     protected
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    end
+
+    private
+
+    def set_layout
+        if devise_controller? && controller_name == "sessions" && action_name == "new"
+            "login"
+        else
+            "application"
+        end
     end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /links or /links.json
   def index
